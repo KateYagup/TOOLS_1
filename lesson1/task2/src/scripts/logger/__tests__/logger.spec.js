ac77d29aed4c87a9d0fbdbc9a3e92e76d9500f90
kate <kata3140@gmail.com>
2024-06-27 10:53:44 +0300

    task1

---
+++ lesson1/task2/src/scripts/logger/__tests__/logger.spec.js
@@ -0,0 +1,14 @@
+import { createLogger } from '../logger';
+
+it('should return stored logs', () => {
+    const logger = createLogger('user login');
+
+    expect(logger.getLogs()).toEqual([]);
+});
+
+it('should save log message', () => {
+    const logger = createLogger('user login');
+    logger.log('login success');
+
+    expect(logger.getLogs()).toEqual(['log - user login - login success']);
+})
