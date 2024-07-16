c2c7a82f523fbbaca08b7939b9f2b3471744a853
kate <kata3140@gmail.com>
2024-07-16 17:12:46 +0300

    task2

--- lesson7/task2/webpack.config.js
+++ lesson7/task2/webpack.config.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -6,8 +9,12 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /.js$/,
+                use: ['babel-loader']
+            },
+            {
                 test: /.s?css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
                 test: /.(jpg|png)$/,
@@ -22,5 +29,14 @@ module.exports = {
                 ],
             }
         ],
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
 };
